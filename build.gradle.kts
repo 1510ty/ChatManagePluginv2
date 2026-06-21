9930cff803e01af7d303d53d56dee7e580653379
1510ty <1510ty2580@gmail.com>
2026-06-21 16:47:49 +0900

    問題なさそうなので1.0.0リリース

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ val buildTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd
 version = if (!gitCommit.isNullOrBlank()) {
     "$buildTime-$gitCommit"
 } else {
-    "0.0.6"
+    "1.0.0"
 }
 
 group = "com.mc1510ty"
